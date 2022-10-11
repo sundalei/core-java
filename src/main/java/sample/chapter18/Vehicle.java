74538ea3500c4496ea9290c714b0a5f69565ab77
DaLei Sun <sundalei2011@163.com>
2022-10-11 23:08:34 +0800

    Chapter 18 Interface

---
+++ src/main/java/sample/chapter18/Vehicle.java
@@ -0,0 +1,12 @@
+package sample.chapter18;
+
+public interface Vehicle {
+
+    public void moveForward(int milesPerHour);
+    public void moveBackward(int milesPerHour);
+    public void stop();
+    public void turnLeft();
+    public void turnRight();
+    public void engineOn();
+    public void engineOff();
+}
