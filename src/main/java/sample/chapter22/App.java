292d127cd5a9c66942e4a15d4d800bed68965a74
DaLei Sun <sundalei1988@gmail.com>
2022-10-14 21:24:29 +0800

    chapter 22

---
+++ src/main/java/sample/chapter22/App.java
@@ -0,0 +1,11 @@
+package sample.chapter22;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+        System.out.println(strings.size());
+    }
+}
